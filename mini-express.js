28017770de62e445eb1f0b4fbdd1a69c3c0202f1
sohaibalidev <msohaibaliofficial@gmail.com>
2025-07-08 11:11:51 +0500

    update

--- mini-express.js
+++ mini-express.js
@@ -1,106 +1,141 @@
 class MiniExpress {
-    #http = require('http')
-    #notfoundfunc;
-    #routes;
+  #http = require("http");
+  #notfoundfunc;
+  #routes;
+  #middlewares;
 
-    constructor() {
-        this.#routes = {};
-        this.#notfoundfunc = null;
+  constructor() {
+    this.#routes = {};
+    this.#middlewares = [];
+    this.#notfoundfunc = null;
+  }
+
+  getRoutes() {
+    console.log("Registered Routes:");
+    for (const [method, routes] of Object.entries(this.#routes)) {
+      console.log(`\n${method}:`);
+      for (const [path] of Object.entries(routes)) {
+        console.log(`-> [ ${path} ]`);
+      }
+    }
+  }
+
+  use(middleware) {
+    if (!middleware || typeof middleware !== "function")
+      throw new Error("middleware must be a function");
+
+    this.#middlewares.push(middleware);
+  }
+
+  #attachHelpers(res) {
+    res.send = (data) => {
+      if (typeof data !== "string" && typeof data !== "object")
+        throw new Error("Response must be a string or object");
+
+      if (typeof data === "string") res.setHeader("Content-Type", "text/plain");
+      else {
+        res.setHeader("Content-Type", "application/json");
+        data = JSON.stringify(data);
+      }
+      res.end(data);
+    };
+
+    res.status = (code) => {
+      res.statusCode = code;
+      return res;
+    };
+
+    res.exitWith = (code) => {
+      res.statusCode = code;
+      res.end();
+    };
+
+    res.json = (obj) => {
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify(obj));
+    };
+  }
+
+  #methodHandler(method, path, handlers) {
+    if (!path) throw new Error("Path is not defined");
+    if (!handlers) throw new Error("Handler is not defined");
+
+    if (!this.#routes[method]) this.#routes[method] = {};
+    this.#routes[method][path] = handlers;
+  }
+
+  #requestParser(req) {
+    if (!req) return;
+    let [path, rawQuery] = req.url.split("?");
+    let parsedQuery = {};
+
+    if (rawQuery) {
+      parsedQuery = Object.fromEntries(
+        rawQuery.split("&").map((q) => {
+          const [key, value = ""] = q.split("=");
+          try {
+            return [key, JSON.parse(decodeURIComponent(value))];
+          } catch {
+            return [key, decodeURIComponent(value)];
+          }
+        })
+      );
     }
 
-    getRoutes() {
-        console.log('Registered Routes:');
-        for (const [method, routes] of Object.entries(this.#routes)) {
-            console.log(`\n${method}:`);
-            for (const [path] of Object.entries(routes)) {
-                console.log(`-> [ ${path} ]`);
-            }
-        }
+    req.path = path;
+    req.query = parsedQuery;
+  }
+
+  #runMiddleware(req, res, routeHandlers) {
+    let globalIndex = 0;
+    const runGlobalMiddleware = () => {
+      if (globalIndex < this.#middlewares.length)
+        this.#middlewares[globalIndex++](req, res, runGlobalMiddleware);
+
+      else runRouteHandlers();
     }
 
-    #attachHelpers(res) {
-        res.send = (data) => {
-            if (typeof data !== 'string' && typeof data !== 'object')
-                throw new Error('Response must be a string or object');
-
-            if (typeof data === 'string') res.setHeader('Content-Type', 'text/plain');
-            else {
-                res.setHeader('Content-Type', 'application/json');
-                data = JSON.stringify(data);
-            }
-            res.end(data);
-        };
-
-        res.status = (code) => {
-            res.statusCode = code;
-            return res;
-        };
-
-        res.exitWith = (code) => {
-            res.statusCode = code;
-            res.end()
-        }
-
-        res.json = (obj) => {
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify(obj));
-        };
+    let routeIndex = 0;
+    const runRouteHandlers = () => {
+      if (routeIndex < routeHandlers.length)
+        routeHandlers[routeIndex++](req, res, runRouteHandlers);
     }
 
-    #methodHandler(method, path, handler) {
-        if (!path) throw new Error('Path is not defined')
-        if (!handler) throw new Error('Handler is not defined')
-        if (!this.#routes[method]) this.#routes[method] = {};
-        this.#routes[method][path] = handler;
-    }
+    runGlobalMiddleware();
+  }
 
-    #requestParser(req) {
-        if (!req) return;
-        let [path, rawQuery] = req.url.split('?');
-        let parsedQuery = {}
+  #requestHandler(req, res) {
+    const { method, path } = req;
+    const routeHandler = this.#routes[method]?.[path];
 
-        if (rawQuery) {
-            parsedQuery = Object.fromEntries(
-                rawQuery.split('&').map(q => {
-                    const [key, value = ''] = q.split('=');
-                    try { return [key, JSON.parse(decodeURIComponent(value))]; }
-                    catch { return [key, decodeURIComponent(value)]; }
-                })
-            );
-        }
+    if (routeHandler)
+      return this.#runMiddleware(req, res, routeHandler)
 
-        req.path = path;
-        req.query = parsedQuery;
-    }
+    if (typeof this.#notfoundfunc === "function")
+      return this.#notfoundfunc(req, res);
 
-    #requestHandler(req, res) {
-        const { method, path } = req;
-        const routeHandler = this.#routes[method]?.[path];
+    return res.status(404).send("404 Not Found");
+  }
 
-        if (routeHandler) return routeHandler(req, res)
-        if (typeof this.#notfoundfunc === 'function') return this.#notfoundfunc(req, res)
-        return res.status(404).send('404 Not Found')
-    }
+  get = (path, ...handler) => this.#methodHandler("GET", path, handler);
+  post = (path, ...handler) => this.#methodHandler("POST", path, handler);
+  put = (path, ...handler) => this.#methodHandler("PUT", path, handler);
+  patch = (path, ...handler) => this.#methodHandler("PATCH", path, handler);
+  delete = (path, ...handler) => this.#methodHandler("DELETE", path, handler);
 
-    get = (path, handler) => this.#methodHandler('GET', path, handler)
-    post = (path, handler) => this.#methodHandler('POST', path, handler)
-    put = (path, handler) => this.#methodHandler('PUT', path, handler)
-    patch = (path, handler) => this.#methodHandler('PATCH', path, handler)
-    delete = (path, handler) => this.#methodHandler('DELETE', path, handler)
+  notFound404 = (callback) => (this.#notfoundfunc = callback);
 
-    notFound404 = (callback) => this.#notfoundfunc = callback;
+  listen(port, callback) {
+    const server = this.#http.createServer((req, res) => {
+      this.#attachHelpers(res);
+      this.#requestParser(req);
+      this.#requestHandler(req, res);
+    });
 
-    listen(port, callback) {
-        const server = this.#http.createServer((req, res) => {
-            this.#attachHelpers(res);
-            this.#requestParser(req);
-            this.#requestHandler(req, res);
-        });
-
-        server.listen(port, () => {
-            if (callback) callback();
-        });
-    }
+    server.listen(port, () => {
+      if (callback) callback();
+    });
+  }
 }
 
-module.exports = MiniExpress
+module.exports = MiniExpress;
--- server.js
+++ server.js
@@ -1,38 +1,72 @@
 const MiniExpress = require('./mini-express.js');
 const app = new MiniExpress();
 
-// Root route with plain string response
+// Middlewares
+app.use((req, res, next) => {
+    console.log('\n\nThis Is Global Middleware')
+    next();
+})
+
+// ================== Middlewares ==================
+
+const checkLoginStatus = (req, res, next) => {
+    const loggedIn = req.headers.isloggedin === "true";
+    
+    if (loggedIn) {
+        console.log('Already logged in');
+        return res.status(400).send('Already Logged In');
+    }
+
+    console.log('User not logged in');
+    next();
+};
+
+const validateLoginFields = (req, res, next) => {
+    // Mocking user input
+    req.body = { username: 'sohaib', password: '123456' };
+
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        console.log('Missing login fields');
+        return res.status(400).json({ error: 'Username and password are required' });
+    }
+
+    console.log('Login fields look good');
+    next();
+};
+
+// ================== Login Route ==================
+
+app.post('/login', checkLoginStatus, validateLoginFields, (req, res) => {
+    console.log('/login hit!');
+    res.status(200).send('Logged In');
+});
+
 app.get('/', (req, res) => {
     res.send('Welcome to MiniExpress!');
 });
 
-// JSON response route
 app.get('/user', (req, res) => {
     res.json({ username: 'sohaib', country: 'Pakistan' });
 });
 
-// Status chaining response
 app.get('/status', (req, res) => {
     res.status(204).send();
 });
 
-// Route testing query params
-// Example: GET /query?name=sohaib&age=18
 app.get('/query', (req, res) => {
     res.json({ receivedQuery: req.query });
 });
 
-// Custom exit with status code
 app.get('/exit-test', (req, res) => {
     res.exitWith(403);
 });
 
-// POST example with simple response
 app.post('/submit', (req, res) => {
     res.status(201).send('Post received');
 });
 
-// Route not found will trigger 404
 app.notFound404((req, res) => {
     const { method, path } = req;
     const jsonMsg = { error: "Route Not Found", method, path }
@@ -40,7 +74,6 @@ app.notFound404((req, res) => {
     console.log(jsonMsg)
     res.status(404).json(jsonMsg);
 });
-
 
 // Start the server
 app.listen(3000, () => {
